d70b16611b948a6b83e6d2bf2833aeabe9477863
trestletech <jeff.allen@trestletechnology.net>
2014-01-18 10:54:57 -0600

    Created an initial squaring function.

---
+++ square.R
@@ -0,0 +1,8 @@
+
+square <- function(x){
+  sum <- 0
+  for (i in 1:x){
+    sum <- sum + x
+  }
+  return(sum)
+}
---
+++ test.R
@@ -0,0 +1,6 @@
+
+test_that("Squaring works", {
+  expect_equal(square(5), 25)
+  
+  expect_equal(square(3), 9)
+})
